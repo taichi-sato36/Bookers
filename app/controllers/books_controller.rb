7111126cab670c1bdb6c4221cfc69004ad8d0407
Taichi Satou <taichi@mpb.local>
2020-04-15 05:52:53 +0900

    [finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,14 +5,19 @@ class BooksController < ApplicationController
   end
 
   def index
-  	@book = Book.new
   	@books = Book.all
+  	@book = Book.new
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to show_book_path(book.id)
+  	@book = Book.new(book_params)
+  	@books = Book.all
+  	if @book.save
+  		flash[:notice] = "Book was successfully created."
+  		redirect_to show_book_path(@book.id)
+    else
+    	render "index"
+    end
   end
 
   def show
@@ -27,9 +32,13 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to show_book_path(book.id)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		flash[:notice] = "Book was successfully updated."
+  		redirect_to show_book_path(@book.id)
+  	else
+  		render "edit"
+  	end
   end
 
   def destroy
